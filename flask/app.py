dd6cf33afba026c80bdc7f3ed091106d6689b25b
wkchiuea <wkchiuea@gmail.com>
2024-05-26 12:09:06 +0800

    feat: create flask api server connect to db

--- flask/app.py
+++ flask/app.py
@@ -1,37 +1,60 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
-from config_file import configs as scrape_configs
+from pymongo import MongoClient
+from datetime import datetime
+import logging
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
 api = Api(app)
 
+client = MongoClient('mongodb://localhost:27017/')
+db = client['raw_news']
+news_data_collection = db['news_data']
+job_log_collection = db['job_log']
 
-configs = {c['name']: c for c in scrape_configs}
-
-
-class Config(Resource):
-
-    def get(self, platform):
-
-        if platform == "all":
-            return {'configs': configs}, 200
-        if platform in configs.keys():
-            return {'config': configs[platform]}, 200
-
-        return {'config': None}, 404
+logging.basicConfig(filename='app.log', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-class RawNews(Resource):
+class NewsResource(Resource):
+    def get(self):
+        """
+        GET endpoint to query news data.
+        Accepts query parameters to filter the results.
+        """
+        query_params = request.args.to_dict()
 
-    def get(self, platform):
-        return {'data': None}, 404
+        # Initialize the MongoDB query
+        query = {}
+
+        # Handle datetime filtering for scrapetime
+        if 'before_datetime' in query_params:
+            before_datetime = datetime.strptime(query_params['before_datetime'], "%Y-%m-%d %H:%M")
+            query['scrapetime'] = {"$lt": before_datetime}
+
+        if 'after_datetime' in query_params:
+            after_datetime = datetime.strptime(query_params['after_datetime'], "%Y-%m-%d %H:%M")
+            query['scrapetime'] = {"$gt": after_datetime}
+
+        if 'start_datetime' in query_params and 'end_datetime' in query_params:
+            start_datetime = datetime.strptime(query_params['start_datetime'], "%Y-%m-%d %H:%M")
+            end_datetime = datetime.strptime(query_params['end_datetime'], "%Y-%m-%d %H:%M")
+            query['scrapetime'] = {
+                "$gte": start_datetime,
+                "$lte": end_datetime
+            }
+
+        # Query the MongoDB collection
+        result = list(news_data_collection.find(query, {"_id": 0}))
+
+        return jsonify(result)
 
 
-api.add_resource(Config, '/config/<string:platform>')
-api.add_resource(RawNews, '/news/<string:platform>')
+# Add the NewsResource to the API
+api.add_resource(NewsResource, '/news')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
