bcf4812b58361f0cb2f44f7136fe6f341a6bc2d1
wkchiuea <wkchiuea@gmail.com>
2024-05-30 22:10:58 +0800

    test: minor update

--- aws/ec2-start.sh
+++ aws/ec2-start.sh
@@ -5,6 +5,7 @@
 sudo yum update -y
 sudo yum install -y tzdata
 sudo yum install -y git
+sudo yum install -y logrotate
 
 sudo yum install -y \
     alsa-lib \
@@ -81,8 +82,27 @@ EOF
 
 # Set up a cron job to run the scripts every hour
 # Add the cron job to the crontab
-(crontab -l 2>/dev/null; echo "0 * * * * /usr/bin/python3 /home/ec2-user/myrepo/web-scraping/web_scraping_v2.py") | crontab -
+echo "30 * * * * /usr/bin/python3 /home/ec2-user/myrepo/web-scraping/web_scraping_v2.py >> /home/ec2-user/myrepo/web-scraping/web_scraping_v2.log 2>&1" | crontab -u ec2-user -
 #(crontab -l 2>/dev/null; echo "30 * * * * /home/ec2-user/myrepo/venv/bin/python /home/ec2-user/myrepo/web-scraping/fb_comment.py 25 --comments_limit 50") | crontab -
+
+# Create the logrotate configuration for the log file
+cat <<EOF > /etc/logrotate.d/web_scraping_v2
+/home/ec2-user/myrepo/web-scraping/web_scraping_v2.log {
+    daily
+    rotate 7
+    compress
+    missingok
+    notifempty
+    create 0644 ec2-user ec2-user
+    postrotate
+        /usr/bin/systemctl reload crond > /dev/null 2>&1 || true
+    endscript
+}
+EOF
+
+# Ensure correct permissions for the logrotate configuration
+chown root:root /etc/logrotate.d/web_scraping_v2
+chmod 644 /etc/logrotate.d/web_scraping_v2
 
 # Print a message indicating that the setup is complete
 echo "Setup complete. The web_scraping_v2.py script will run every hour."
